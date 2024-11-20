ff86572794058082acf7e3b34bdf2a1f39bbe0d4
Jaydee-31 <johndextergamo@gmail.com>
2024-11-21 05:17:39 +0800

    register FilamentColor
    Signed-off-by: Jaydee-31 <johndextergamo@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        FilamentColor::register([
+            'danger' => Color::Red,
+            'gray' => Color::Zinc,
+            'info' => Color::Blue,
+            'primary' => Color::Blue,
+            'success' => Color::Green,
+            'warning' => Color::Amber,
+        ]);
     }
 
     /**
