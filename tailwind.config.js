cf017779e85fd30f293553d7dac909e529fb0b01
Jaydee-31 <johndextergamo@gmail.com>
2024-11-21 04:55:31 +0800

    installed postcss-nesting
    Signed-off-by: Jaydee-31 <johndextergamo@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import preset from './vendor/filament/support/tailwind.config.preset'
 
 /** @type {import('tailwindcss').Config} */
 export default {
