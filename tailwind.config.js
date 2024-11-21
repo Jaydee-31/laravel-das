6f5cc0e2a4b6bd8df1ce2432b12fe1f864dc7b77
Jaydee-31 <johndextergamo@gmail.com>
2024-11-21 14:19:08 +0800

    wip
    Signed-off-by: Jaydee-31 <johndextergamo@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-import preset from './vendor/filament/support/tailwind.config.preset'
 import defaultTheme from 'tailwindcss/defaultTheme';
+import preset from './vendor/filament/support/tailwind.config.preset'
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
 export default {
     presets: [preset],
+    darkMode: 'class',
     content: [
         './app/Filament/**/*.php',
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
+
     theme: {
         extend: {
             fontFamily: {
@@ -14,5 +20,6 @@ export default {
             },
         },
     },
-}
 
+    plugins: [forms, typography],
+};
