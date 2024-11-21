d4d22717359c5ca2fff7372638343a4fb8368c47
Jaydee-31 <johndextergamo@gmail.com>
2024-11-22 03:01:20 +0800

    wip
    Signed-off-by: Jaydee-31 <johndextergamo@gmail.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-bold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Banners & Graphics Naming Generator') }}
+            {{ __('Calendar') }}
         </h2>
     </x-slot>
 
     <div class="py-3">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @livewire('user-list')
-            @livewire('banner-naming')
+            <livewire:appointments-calendar/>
         </div>
     </div>
 </x-app-layout>
