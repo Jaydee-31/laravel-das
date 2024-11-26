4ed22173563c1d2950b0d1c5cb1bca2db30f8261
Jaydee-31 <johndextergamo@gmail.com>
2024-11-27 05:55:41 +0800

    Added: current date on top-bar
    Signed-off-by: Jaydee-31 <johndextergamo@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
 
-    <meta name="application-name" content="{{ config('app.name') }}" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="application-name" content="{{ config('app.name') }}"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
     <title>{{ config('app.name') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=poppins:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=poppins:400,500,600&display=swap" rel="stylesheet"/>
 
     <style>
         [x-cloak] {
@@ -44,11 +44,11 @@
         <div class="bg-white h-20 w-full flex items-center justify-center mb-4">
             <!-- Logo -->
             <a href="{{ route('dashboard') }}">
-                <x-application-mark class="h-9 w-auto" />
+                <x-application-mark class="h-9 w-auto"/>
             </a>
         </div>
 
-            @livewire('navigation-menu')
+        @livewire('navigation-menu')
     </aside>
 
     <!-- Overlay (only shown on small screens when sidebar is open) -->
@@ -58,13 +58,34 @@
 
     <!-- Main Content -->
     <div class="flex-1 flex flex-col w-full ">
-        <header class="fixed w-full h-16 bg-white border-b flex  items-center px-4">
-            <button @click="isSidebarOpen = !isSidebarOpen" class="p-2 focus:outline-none lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6h16.5M3.75 12h16.5M3.75 18h16.5" />
-                </svg>
-            </button>
-            <h1 class="ml-4 text-md font-light">Doctors Appointment System Bethel Baptist Hospital Inc.</h1>
+        <header class="flex h-16 w-full gap-4 justify-between md:px-4 px-2 border-b">
+            <div class="flex h-full items-center">
+                <button @click="isSidebarOpen = !isSidebarOpen" class="p-2 focus:outline-none lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M3.75 6h16.5M3.75 12h16.5M3.75 18h16.5"/>
+                    </svg>
+                </button>
+            </div>
+            <h1 class="flex-grow self-center hidden md:block">Doctors Appointment System Bethel Baptist
+                Hospital Inc.</h1>
+
+            <a class="flex gap-3 lg:me-4 me-2 cursor-pointer" href="{{ route('dashboard') }}">
+                <div class="flex h-full justify-center items-end flex-col">
+                    <p class="font-light text-xs">Today's date</p>
+                    <h4 class="font-medium text-lg tracking-wider">{{Carbon\Carbon::now()->toDateString("Y-m-d")}}</h4>
+                </div>
+                <div class="flex h-full justify-center items-center">
+                    <div class="p-2 bg-primary-100 border rounded">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"/>
+                        </svg>
+                    </div>
+                </div>
+            </a>
         </header>
 
         <main class="flex-1 mt-16 overflow-y-auto">
