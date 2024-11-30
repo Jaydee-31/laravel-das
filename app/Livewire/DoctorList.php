9db7711b75492f62699a7dc183c712537d319490
Jaydee-31 <johndextergamo@gmail.com>
2024-12-01 03:52:07 +0800

    Added schedule form
    Signed-off-by: Jaydee-31 <johndextergamo@gmail.com>

--- app/Livewire/DoctorList.php
+++ app/Livewire/DoctorList.php
@@ -7,15 +7,17 @@ use App\Models\User;
 use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
-use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\TimePicker;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Get;
 use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\ImageColumn;
@@ -48,9 +50,76 @@ class DoctorList extends Component implements HasForms, HasTable, HasActions
         return \Filament\Actions\CreateAction::make()
             ->label('Add new doctor')
             ->model(Doctor::class)
-            ->form($this->doctorForm())
+            ->form([
+                Tabs::make('Tabs')
+                    ->tabs([
+                        Tabs\Tab::make('Tab 1')
+                            ->schema(
+                                $this->doctorForm()
+                            ),
+                        Tabs\Tab::make('Tab 2')
+                            ->schema([
+                                Repeater::make('schedules')
+                                    ->relationship()
+                                    ->schema([
+                                        Select::make('day')
+                                            ->options([
+                                                'Monday' => 'Monday',
+                                                'Tuesday' => 'Tuesday',
+                                                'Wednesday' => 'Wednesday',
+                                                'Thursday' => 'Thursday',
+                                                'Friday' => 'Friday',
+                                                'Saturday' => 'Saturday',
+                                                'Sunday' => 'Sunday',
+                                            ])
+                                            ->reactive()
+                                            ->disableOptionWhen(function ($value, $state, Get $get) {
+                                                return collect($get('../*.day'))
+                                                    ->reject(fn($selected) => $selected === $state)
+                                                    ->filter()
+                                                    ->contains($value);
+                                            })
+                                            ->required()
+                                            ->label('Day'),
+
+                                        // Set start time
+                                        TimePicker::make('start_time')
+                                            ->required()
+                                            ->seconds(false)
+                                            ->label('Start Time'),
+
+                                        // Set end time
+                                        TimePicker::make('end_time')
+                                            ->required()
+                                            ->seconds(false)
+                                            ->label('End Time'),
+
+                                        Select::make('week')
+                                            ->multiple()
+                                            ->options([
+                                                '1' => '1st Week',
+                                                '2' => '2nd Week',
+                                                '3' => '3rd Week',
+                                                '4' => '4th Week',
+                                                '5' => '5th Week',
+                                            ])
+                                            ->label('Applicable Weeks')
+                                            ->placeholder('Select weeks')
+                                            ->helperText('Choose which weeks of the month this schedule applies to')
+                                            ->required()
+                                            ->columnSpan(2),
+
+                                        // Toggle for "By Appointment"
+                                        Toggle::make('by_appointment')
+                                            ->label('By Appointment')
+                                            ->default(false),
+                                    ])->defaultItems(3)->cloneable()->columns(3),
+                            ]),
+                    ])
+            ])
             ->mutateFormDataUsing(function (array $data): array {
 //
+//                dd($data);
                 $user = User::create([
                     'name' => $data['name'],
                     'username' => $data['username'],
@@ -117,7 +186,7 @@ class DoctorList extends Component implements HasForms, HasTable, HasActions
 
                         // Update the doctor-specific fields
                         $data['user_id'] = $record->user_id; // Ensure user_id is retained
-                        unset($data['name'], $data['username'],  $data['email'], $data['password'], $data['profile_photo_path']); // Remove non-doctor fields
+                        unset($data['name'], $data['username'], $data['email'], $data['password'], $data['profile_photo_path']); // Remove non-doctor fields
 
                         return $data;
                     })
@@ -142,7 +211,8 @@ class DoctorList extends Component implements HasForms, HasTable, HasActions
         dd('asdad');
     }
 
-    public function doctorForm(): array {
+    public function doctorForm(): array
+    {
 
         return [
             Group::make([
@@ -156,24 +226,24 @@ class DoctorList extends Component implements HasForms, HasTable, HasActions
                             ->imageCropAspectRatio('1:1')
                             ->maxSize(2048)
                             ->panelAspectRatio('1:1')
-                    ])->columnSpan([ 'xl' => 2]),
+                    ])->columnSpan(['xl' => 2]),
 
                 Group::make()
                     ->schema([
                         TextInput::make('name')
-                            ->dehydrateStateUsing(fn ($state) => ucwords($state))
+                            ->dehydrateStateUsing(fn($state) => ucwords($state))
                             ->required()
                             ->maxLength(255)
                             ->columnSpan(2),
                         TextInput::make('username')
-                            ->rule(fn ($component) => Rule::unique('users', 'username')
+                            ->rule(fn($component) => Rule::unique('users', 'username')
                                 ->ignore($component->getRecord()->user_id ?? null, 'id'))
                             ->required()
                             ->maxLength(25)
                             ->autocomplete(false)
                             ->columnSpan(1),
                         TextInput::make('email')
-                            ->rule(fn ($component) => Rule::unique('users', 'email')
+                            ->rule(fn($component) => Rule::unique('users', 'email')
                                 ->ignore($component->getRecord()->user_id ?? null, 'id'))
                             ->email()
                             ->autocomplete(false)
@@ -190,11 +260,9 @@ class DoctorList extends Component implements HasForms, HasTable, HasActions
                         TextInput::make('password')
                             ->password()
                             ->maxLength(255)
-                            ->dehydrateStateUsing(static fn (null|string $state): null|string =>
-                            filled($state) ? Hash::make($state) : null,
+                            ->dehydrateStateUsing(static fn(null|string $state): null|string => filled($state) ? Hash::make($state) : null,
                             )
-                            ->dehydrated(static fn (null|string $state): bool =>
-                            filled($state),
+                            ->dehydrated(static fn(null|string $state): bool => filled($state),
                             )
                             ->columnSpan(1),
                         TextInput::make('passwordConfirmation')
@@ -204,9 +272,15 @@ class DoctorList extends Component implements HasForms, HasTable, HasActions
                             ->columnSpan(1),
                     ])
                     ->columns(2)
-                    ->columnSpan([ 'xl' => 3])
-            ])->columns([ 'xl' => 5]),
+                    ->columnSpan(['xl' => 3])
+            ])->columns(['xl' => 5]),
         ];
     }
 
+    public function scheduleForm(): array
+    {
+        return [
+            //
+        ];
+    }
 }
