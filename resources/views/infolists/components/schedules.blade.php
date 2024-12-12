da24813f7e1d3b1ebef1ca49c9c48149004477c9
Jaydee-31 <johndextergamo@gmail.com>
2024-12-13 04:28:20 +0800

    bug fix
    Signed-off-by: Jaydee-31 <johndextergamo@gmail.com>

--- resources/views/infolists/components/schedules.blade.php
+++ resources/views/infolists/components/schedules.blade.php
@@ -1,15 +1,19 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
     <div class="flex flex-col text-sm leading-6 text-gray-950 dark:text-white">
-        @foreach($getState() as $schedule)
-            <div class="flex">
-                <p class="font-bold"> {{ $schedule->day}}</p>
-                @if($schedule->start_time)
-                    <p>{{ date(': g:i A - ', strtotime($schedule->start_time)) . date('g:i A', strtotime($schedule->end_time))}}</p>
-                @else
-                    <p class="italic">: By Appointment</p>
-                @endif
-            </div>
-        @endforeach
+        @if($getState())
+            @foreach($getState() as $schedule)
+                <div class="flex">
+                    <p class="font-bold"> {{ $schedule->day}}</p>
+                    @if($schedule->start_time)
+                        <p>{{ date(': g:i A - ', strtotime($schedule->start_time)) . date('g:i A', strtotime($schedule->end_time))}}</p>
+                    @else
+                        <p class="italic">: By Appointment</p>
+                    @endif
+                </div>
+            @endforeach
+        @else
+            <p class="italic">No schedules found.</p>
+        @endif
         {{--        {{ $getState() }}--}}
         {{--        {{ $getRecord()}}--}}
     </div>
